e471c3f230da2a909090ab0e21ac872296eeaf7c
juliendira <julien.dirassouyan@outlook.fr>
2025-06-01 16:17:59 +0200

    transition vers CRA

--- services/frontend/tailwind.config.js
+++ services/frontend/tailwind.config.js
@@ -1,26 +1,8 @@
-// tailwind.config.js
-export default {
-  // Active Tailwind sur tous tes fichiers sources
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-
-  // Thème personnalisé
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        // Couleur de fond semi-transparente pour l'effet glassmorphism
-        "glass-dark": "rgba(255, 255, 255, 0.05)",
-      },
-      boxShadow: {
-        // Ombre interne pour les inputs gravés dans le fond
-        "inner-glass":
-          "inset 4px 4px 10px rgba(0,0,0,0.4), inset -4px -4px 10px rgba(255,255,255,0.05)",
-      },
-    },
+    extend: {},
   },
-
-  // Active le mode sombre basé sur une classe .dark (à gérer côté React)
-  darkMode: "class",
-
-  // Plugins éventuels à ajouter plus tard (forms, typography…)
   plugins: [],
-};
+}
